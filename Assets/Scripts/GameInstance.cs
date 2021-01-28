45ed27155f737310f407e9750e3853232d4bdbef
AlvinJ <alvinjia66@gmail.com>
2021-01-29 07:58:52 +0800

    Hook Placeholder and Adding game instance

---
+++ Assets/Scripts/GameInstance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstance : MonoBehaviour
+{
+    public static GameInstance Instance { get; private get; }
+
+    public int CurrentEmployee = 0;
+    public int WorkSpace = 5;
+    public int Money = 0;
+    public int LevelMoney = 0;
+    public float Timer = 0.0;
+    public float LevelTime = 0.0;
+    public bool Iffull = false;
+
+    public int DeadEmployee = 0;
+    //public 
+
+}
