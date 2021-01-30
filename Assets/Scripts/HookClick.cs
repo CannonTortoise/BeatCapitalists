baabb2461af9098f8880248f924da67f8e84d6dc
ruiw2006 <ruiw2006@126.com>
2021-01-31 03:18:30 +0800

    Changed hook collider

---
+++ Assets/Scripts/HookClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookClick : MonoBehaviour
+{
+    private HookScript hook;
+
+    private void Start()
+    {
+        hook = GetComponentInChildren<HookScript>();
+    }
+
+    private void OnMouseDown()
+    {
+        hook.BeClicked();
+    }
+}
