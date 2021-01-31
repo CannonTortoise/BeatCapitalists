b7ee9e758685d5041ecc563c68f4c8f6e03143f9
AlvinJ <alvinjia66@gmail.com>
2021-01-31 20:32:53 +0800

    PauseMenu Added

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+    public Text[] Textboxes = new Text[7];
+
+    void Start()
+    {
+        for (int i = 0; i < Textboxes.Length; i++)
+        {
+            Textboxes[i] = GameObject.Find(("PauseText" + i)).GetComponent<Text>();
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < (AchievementList.Instance.Achievmenetlist.Length - 1); i++)
+        {
+            if (AchievementList.Instance.Achievmenetlist[i] == statues.Achieve)
+            {
+                Color z = Textboxes[i].color;
+                z.a = 1.0f;
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
